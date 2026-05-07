51bedac4ad95368c52868e4cd0f4ce14ed9c97ee
xdarwin13 <ddavidperez@uniguajira.edu.co>
2026-05-07 09:19:33 -0500

    Add ProfilePage route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import CheckoutPage from './views/CheckoutPage/CheckoutPage';
 import Layout from './components/Layout/LayoutComponents';
 
-
+import ProfilePage from './views/ProfilePage/ProfilePage';
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<CheckoutPage />} />
         <Route path="checkout" element={<CheckoutPage />} />
+        <Route path="profile" element={<ProfilePage />} />
       </Route>
     </Routes>
   )
