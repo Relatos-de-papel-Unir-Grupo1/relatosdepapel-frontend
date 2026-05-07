0fa6566b24609c53b2c760925b7591f64939b370
Darwin David Perez Muñoz <161097866+xdarwin13@users.noreply.github.com>
2026-05-07 18:51:24 -0500

    Add ProductDetailsPage route to App component (#5)
    Co-authored-by: SebastianPrieto <Sebas96061206601@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css'
 import CheckoutPage from './views/CheckoutPage/CheckoutPage';
 import Layout from './components/Layout/LayoutComponents';
+import ProductDetailsPage from './views/ProductDetailsPage/ProductDetailsPage';
 import HomePage from './views/HomePage/HomePage';
 
 
 function App() {
   return (
     <Routes>
-  <Route path="/" element={<Layout />}>
-    <Route index element={<CheckoutPage />} />
-    <Route path="home" element={<HomePage />} />
-    <Route path="checkout" element={<CheckoutPage />} />
-  </Route>
-</Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<CheckoutPage />} />
+        <Route path="checkout" element={<CheckoutPage />} />
+        <Route path="home" element={<HomePage />} />
+        <Route path="product/:id" element={<ProductDetailsPage />} />
+      </Route>
+    </Routes>  
   )
 }
 
